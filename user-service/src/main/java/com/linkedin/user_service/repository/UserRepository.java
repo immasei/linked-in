495e5c542bd3e4e5995b82d036e2ad1138eb1220
Lan Linh Nguyen <lanlinh.nguyen@sydney.edu.au>
2025-12-17 02:23:23 +0700

    fix(user-service): missing annotation

--- user-service/src/main/java/com/linkedin/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/linkedin/user_service/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.linkedin.user_service.repository;
 import com.linkedin.user_service.entity.User;
 import com.linkedin.user_service.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
